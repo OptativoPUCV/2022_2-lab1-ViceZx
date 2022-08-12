1ced1d719a540b73e5d4b466b0ad293ad49913c1
Replit user <>
2022-08-12 18:02:01 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -111,7 +111,7 @@ actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
   for(int i = 0; i < 3; i++){
-    scanf("%d",&a);
+    scanf(&a);
     for(int j = 0; j < 3; j++){
       scanf("%d",&b);
     }
